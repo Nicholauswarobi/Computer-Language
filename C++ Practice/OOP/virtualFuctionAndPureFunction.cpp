00717d6afe68a912d6b0ab4853df6b70280ab300
Nicholauswarobi <warobinicholaus@gmail.com>
2025-04-02 03:22:52 +0300

    #

--- C++ Practice/OOP/virtualFuctionAndPureFunction.cpp
+++ C++ Practice/OOP/virtualFuctionAndPureFunction.cpp
@@ -5,10 +5,8 @@ using namespace std;
 // Base class
 class Instrument{
     public:
-        void MakeSound(){
-            cout << "Instrument is playing..." << endl;
-
-        }
+        // For function to be virtual must have virtual keyword
+        virtual void MakeSound() = 0;
 };
 
 class Accordion: public Instrument{
@@ -18,11 +16,20 @@ class Accordion: public Instrument{
         }
 };
 
+class Piano: public Instrument{
+    public:
+        void MakeSound(){
+            cout << "Piano is playing..." << endl;
+    }
+};
+
 int main(){
 
     // Object
     Instrument* I1 = new Accordion();
+    Instrument* I2 = new Piano();
     I1->MakeSound();
+    I2->MakeSound();
     
     system("pause > 0");
     return 0;

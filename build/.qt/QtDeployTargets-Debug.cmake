cd785a71a3df83c396ad79a3583a69eba6d9c513
Nicholauswarobi <warobinicholaus@gmail.com>
2025-04-08 16:35:53 +0300

    #

---
+++ build/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_oneAndOnly_FILE C:/Users/Warobi/Desktop/Computer-Language/build/Debug/oneAndOnly.exe)
+set(__QT_DEPLOY_TARGET_oneAndOnly_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_oneAndOnly_RUNTIME_DLLS C:/Qt_6.8/6.8.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt_6.8/6.8.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt_6.8/6.8.2/msvc2022_64/bin/Qt6Cored.dll)
---
+++ build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_oneAndOnly_FILE C:/Users/Warobi/Desktop/Computer-Language/build/RelWithDebInfo/oneAndOnly.exe)
+set(__QT_DEPLOY_TARGET_oneAndOnly_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_oneAndOnly_RUNTIME_DLLS C:/Qt_6.8/6.8.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt_6.8/6.8.2/msvc2022_64/bin/Qt6Gui.dll;C:/Qt_6.8/6.8.2/msvc2022_64/bin/Qt6Core.dll)

a265da46af7e997dafde21a9ae81c69264244d67
Nicholauswarobi <warobinicholaus@gmail.com>
2025-04-11 15:28:30 +0300

    #

---
+++ Visual Studio Projects/Kilimo/Kilimo/Kilimo.cpp
@@ -0,0 +1,13 @@
+﻿// Kilimo.cpp : Defines the entry point for the application.
+//
+
+#include "Kilimo.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Visual Studio Projects/Project1/Project1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hellow Nungu";
+
+	cin.get();
+}
